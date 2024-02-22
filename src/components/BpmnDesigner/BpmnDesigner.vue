8fa12cf36a58baa5737998bd05b3fcd5a0735950
carry3033 <carry3033@qq.com>
2024-02-22 20:46:06 +0800

    feat: 新增默认xml

--- src/components/BpmnDesigner/BpmnDesigner.vue
+++ src/components/BpmnDesigner/BpmnDesigner.vue
@@ -6,55 +6,70 @@
 
 <script>
 import BpmnModeler from 'bpmn-js/lib/Modeler';
-// 这里是直接引用了xml字符串
-import { xmlStr } from '@/mock/xmlStr'
+import DefaultXML from '@/entity/defaultXml';
 export default {
     name: 'BpmnDesigner',
+    props: {
+        processId: String,
+        processName: String,
+        prefix: {
+            type: String,
+            default: "activiti"
+        },
+    },
     data() {
         return {
-            bpmnModeler: null
-        }
+            bpmnModeler: null,
+        };
     },
     mounted() {
-        this.init()
+        this.init();
     },
     methods: {
-        init () {
+        init() {
             // 获取到属性ref为“canvas”的dom节点
-            const canvas = this.$refs.canvas
+            const canvas = this.$refs.canvas;
             // 建模
             this.bpmnModeler = new BpmnModeler({
-                container: canvas
-            })
-            debugger
-            this.createNewDiagram()
+                container: canvas,
+            });
+            debugger;
+            this.createNewDiagram();
         },
-        createNewDiagram () {
+        // 创建新的流程图
+        async createNewDiagram(xmlStr) {
             // 将字符串转换成图显示出来
-            this.bpmnModeler.importXML(xmlStr, (err) => {
-                if (err) {
-                    console.error(err)
+            let newId = this.processId || `Process_${new Date().getTime()}`;
+            let newName = this.processName || `业务流程_${new Date().getTime()}`;
+            let xmlString = xmlStr || DefaultXML(newId, newName, this.prefix);
+            try {
+                let { warnings } = await this.bpmnModeler.importXML(xmlString);
+                if (warnings && warnings?.length) {
+                    warnings.forEach(warn => console.warn(warn));
                 }
-                else {
-                    // 这里是成功之后的回调, 可以在这里做一系列事情
-                    this.success()
-                }
-            })
-        },
-        success () {
-            console.log('创建成功!')
+            } catch (e) {
+                console.error(`[Process Designer Warn]: ${e.message || e}`);
+            }
+            // this.bpmnModeler.importXML(xmlString, (err) => {
+            //     if (err) {
+            //         console.error(err);
+            //     } else {
+            //         // 这里是成功之后的回调, 可以在这里做一系列事情
+            //         console.log('创建成功!');
+            //     }
+            // });
         }
     }
-}
+};
 </script>
 
 <style scoped>
-.containers{
+.containers {
     background-color: #ffffff;
     width: 100%;
     height: calc(100vh - 52px);
 }
-.canvas{
+.canvas {
     width: 100%;
     height: 100%;
 }
