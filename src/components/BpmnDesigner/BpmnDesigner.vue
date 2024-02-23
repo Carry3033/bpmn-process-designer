e978966f6b47147f8e5cf0c6c21bda7468ca20b8
carry3033 <carry3033@qq.com>
2024-02-23 15:05:13 +0800

    feat: 新增流程设计器背景

--- src/components/BpmnDesigner/BpmnDesigner.vue
+++ src/components/BpmnDesigner/BpmnDesigner.vue
@@ -1,12 +1,13 @@
 <template>
-    <div class="containers">
-        <div class="canvas" ref="canvas"></div>
+    <div class="bpmn-designer__container">
+        <div ref="canvas" :class="['bpmn-designer__canvas', bgCanvas]"></div>
     </div>
 </template>
 
 <script>
 import BpmnModeler from 'bpmn-js/lib/Modeler';
-import DefaultXML from '@/entity/defaultXml';
+import defaultXML from '@/entity/defaultXml';
+
 export default {
     name: 'BpmnDesigner',
     props: {
@@ -22,6 +23,12 @@ export default {
             bpmnModeler: null,
         };
     },
+    computed: {
+        bgCanvas() {
+            // 网状背景
+            return 'nets-canvas';
+        }
+    },
     mounted() {
         this.init();
     },
@@ -33,7 +40,6 @@ export default {
             this.bpmnModeler = new BpmnModeler({
                 container: canvas,
             });
-            debugger;
             this.createNewDiagram();
         },
         // 创建新的流程图
@@ -41,7 +47,7 @@ export default {
             // 将字符串转换成图显示出来
             let newId = this.processId || `Process_${new Date().getTime()}`;
             let newName = this.processName || `业务流程_${new Date().getTime()}`;
-            let xmlString = xmlStr || DefaultXML(newId, newName, this.prefix);
+            let xmlString = xmlStr || defaultXML(newId, newName, this.prefix);
             try {
                 let { warnings } = await this.bpmnModeler.importXML(xmlString);
                 if (warnings && warnings?.length) {
@@ -64,13 +70,16 @@ export default {
 </script>
 
 <style scoped>
-.containers {
+.bpmn-designer__container {
     background-color: #ffffff;
     width: 100%;
-    height: calc(100vh - 52px);
+    height: 100vh;
 }
-.canvas {
+.bpmn-designer__canvas {
     width: 100%;
     height: 100%;
+}
+.nets-canvas {
+    background: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAiIGhlaWdodD0iNDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGRlZnM+PHBhdHRlcm4gaWQ9ImEiIHdpZHRoPSI0MCIgaGVpZ2h0PSI0MCIgcGF0dGVyblVuaXRzPSJ1c2VyU3BhY2VPblVzZSI+PHBhdGggZD0iTTAgMTBoNDBNMTAgMHY0ME0wIDIwaDQwTTIwIDB2NDBNMCAzMGg0ME0zMCAwdjQwIiBmaWxsPSJub25lIiBzdHJva2U9IiNlMGUwZTAiIG9wYWNpdHk9Ii4yIi8+PHBhdGggZD0iTTQwIDBIMHY0MCIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjZTBlMGUwIi8+PC9wYXR0ZXJuPjwvZGVmcz48cmVjdCB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJ1cmwoI2EpIi8+PC9zdmc+") repeat !important;
 }
 </style>
